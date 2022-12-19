93e9ef8d58374c24ce06c7ee6ac6cf110d3591e7
ze <zimwolfgang24@gmail.com>
2022-12-19 11:52:29 -0400

    feat: allowed_hosts add

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bandkamp-deploy-zimwolfgang7k-production.up.railway.app/', "0.0.0.0"]
 
 
 # Application definition
